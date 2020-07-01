d19c08c908ca200b2d1015047bec894ba8a7d255
equusbambusoideae <zhu2qian1@gmail.com>
2020-07-02 01:50:59 +0900

    update t0.js

--- 202007/t0.js
+++ 202007/t0.js
@@ -1,2 +1,15 @@
-const tmsg = "I'm JavaScript!";
-alert(tmsg)
+function pow(x, n) {
+    let result = 1;
+
+    for (let i = 0; i < n; i++){
+        result *= x;
+    }
+    return result;
+}
+
+let x = prompt("x?", "")
+let n = prompt("n?","")
+
+if (n < 0) {
+    alert("Power ${n} is not supported, please enter an integer Number,greater than 0")
+}
